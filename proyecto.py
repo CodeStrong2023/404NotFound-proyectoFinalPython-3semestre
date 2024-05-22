84d76ac11cad10df0671c06c9880db13c1da9759
Gonzalo Balmaceda <www.gonzalobalmaceda@gmail.com>
2024-05-22 14:27:48 -0300

    Agrego las funciones 'validarNumero' y 'agregar_contacto'

--- proyecto.py
+++ proyecto.py
@@ -20,3 +20,36 @@ class Agenda:
             database="agenda_contactos"
         )
         self.cursor = self.db_connection.cursor()
+
+    def validarNumero(self, telefono):
+        while not telefono.isdigit():
+            print("El número de teléfono debe contener números")
+            telefono = input("Ingrese el número de teléfono del contacto: ")
+        return telefono
+        
+    def agregar_contacto(self):
+        while True:
+            nombre = input("Ingrese el nombre del contacto: ")
+            telefono = input("Ingrese el número de teléfono del contacto: ")
+            telefono = self.validarNumero(telefono)
+            email = input("Ingrese el email del contacto: ")
+            
+            if re.match(r"[^@]+@[^@]+\.[^@]+", email):
+                nuevo_contacto = Contacto(nombre, telefono, email)
+                
+                exists = any(contacto.nombre == nombre or contacto.email == email or contacto.telefono == telefono for contacto in self.contactos)
+                if exists:
+                    print("El contacto ya existe.")
+                    return False
+                else:
+                    favorito = input("¿Desea marcar el contacto como favorito? (s/n): ").lower()
+                    if favorito == "s":
+                        nuevo_contacto.favorito = True
+                    elif favorito == "n":
+                        nuevo_contacto.favorito = False
+                    self.contactos.append(nuevo_contacto)
+                    self.guardar_agenda_db(nuevo_contacto)
+                    print("Contacto agregado correctamente.")
+                    return True
+            else:
+                print("El formato del correo electrónico no es válido. Por favor, inténtelo nuevamente.")
